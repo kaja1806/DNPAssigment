7d3812f2198633ea97f3642e86cb059099beb9cc
kaja1806 <weronikakaniowska@gmail.com>
2023-11-05 00:33:30 +0100

    Authorization

--- WebAPI/Services/AuthService.cs
+++ WebAPI/Services/AuthService.cs
@@ -1,27 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿/*using System.ComponentModel.DataAnnotations;
+using Microsoft.Identity.Json;
 using Shared.Models;
 
 namespace WebAPI.Services;
 
 public class AuthService : IAuthService
 {
-    private readonly IList<User> _users = new List<User>
+    private readonly IList<User> _users;
+
+    public AuthService()
     {
-        new()
-        {
-            Password = "kaja18",
-            Role = "user",
-            UserName = "Kaja",
-            SecurityLevel = 4
-        },
-        new()
-        {
-            Password = "luna18",
-            Role = "admin",
-            UserName = "Luna",
-            SecurityLevel = 2
-        }
-    };
+        string usersJson = File.ReadAllText("data.json");
+        _users = JsonSerializer.Deserialize<List<User>>(usersJson);
+    }
+
 
     public Task<User> ValidateUser(string username, string password)
     {
@@ -61,5 +53,69 @@ public class AuthService : IAuthService
         _users.Add(user);
 
         return Task.CompletedTask;
+    }
+}*/
+
+
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using Shared.Models;
+
+namespace WebAPI.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IList<User>? _users;
+
+        public AuthService(IList<User> users)
+        {
+            var usersJson = File.ReadAllText("data.json");
+            _users = JsonSerializer.Deserialize<List<User>>(usersJson);
+        }
+
+        public Task<User> ValidateUser(string username, string password)
+        {
+            if (_users != null)
+            {
+                User? existingUser = _users.FirstOrDefault(u =>
+                    u.UserName.Equals(username, StringComparison.OrdinalIgnoreCase));
+
+                if (existingUser == null)
+                {
+                    throw new Exception("User not found");
+                }
+
+                if (!existingUser.Password.Equals(password))
+                {
+                    throw new Exception("Password mismatch");
+                }
+
+                return Task.FromResult(existingUser);
+            }
+
+            return null;
+        }
+
+        public Task<User> GetUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RegisterUser(User user)
+        {
+            if (string.IsNullOrEmpty(user.UserName))
+            {
+                throw new ValidationException("Username cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(user.Password))
+            {
+                throw new ValidationException("Password cannot be null");
+            }
+
+            _users?.Add(user);
+
+            return Task.CompletedTask;
+        }
     }
 }
